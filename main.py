0645cfaf01c0a8841803d719a57c2afcb911f753
moamen nader <momennader37@gmail.com>
2025-10-21 11:12:55 +0300

    new package

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from typing import Optional
+
+app=FastAPI()
+
+@app.get("/blog")
+def index(limit=10 ,published:bool=True , sort:Optional[str]=None):
+
+    if published:
+        return {'data':f'{limit} published blog from the db'}
+    else:
+        return {'data':f'{limit} blog from the db'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'all unpublished blogs'}
+
+@app.get("/about")
+def about():
+    return {'data':'about page'}
+
+@app.get("/blog/{id}")
+def show(id:int):
+    return {'data': id}
+
+@app.get("/blog/{id}/comments")
+def comments(id:str):
+    """ test"""
+    return {'data':{'1', '2'}}
+
+@app.post('/blog')
+def create_blog():
+    return {'data':'blog is created'}
+
+
+
